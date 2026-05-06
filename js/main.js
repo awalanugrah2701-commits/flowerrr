09cc335c3226ebd0b3d7aa4f2f2317e8f965df7a
walll <awalanugrah2701@gmail.com>
2026-05-06 20:43:00 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('BUNGAA EHH MAAF TERLAMBATT').split('')
+    const titles = ('BUNGA TAA SHAHIRA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
