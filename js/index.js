d12cf5b2bd598b587c4dd65e3f9e71730899cadc
walll <awalanugrah2701@gmail.com>
2026-05-06 20:42:24 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `FLOWERS FOR SHAHIRAAA`.split('')
+const text = `BUNGAAAA TAA SHAHIRA`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
